2434d0f64d42a9aa7a838a83ebe7a7bdca3d571c
Shaun Ashby <Shaun.Ashby@gmail.com>
2015-04-17 23:43:17 +0200

    Modify ciphers in xapissl init script directly.

--- manifests/hardening.pp
+++ manifests/hardening.pp
@@ -86,18 +86,15 @@ class xen::hardening {
   }
 
   # Manage XAPI SSL source config:
-  file { '/etc/xensource/xapi-ssl.conf':
+  file { '/etc/init.d/xapissl':
     ensure => present,
-    group  => 'root',
-    mode   => '0644',
-    owner  => 'root',
   }
 
   # Replace the default SSL ciphers:
-  exec { 'Replace default SSL ciphers in /etc/xensource/xapi-ssl.conf':
-    command => "/bin/sed -i -e 's/ciphers = !SSLv2:RSA+AES256-SHA:RSA+AES128-SHA:RSA+RC4-SHA:RSA+RC4-MD5:RSA+DES-CBC3-SHA/ciphers = AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:HIGH:3DES:!PSK:!MD5:!aNULL:!eNULL/' /etc/xensource/xapi-ssl.conf",
-    unless  => "/bin/grep 'AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:HIGH:3DES:!PSK:!MD5:!aNULL:!eNULL' /etc/xensource/xapi-ssl.conf",
-    require => File['/etc/xensource/xapi-ssl.conf'],
+  exec { 'Replace default SSL ciphers in /etc/init.d/xapissl':
+    command => "/bin/sed -i -e 's/ciphers = !SSLv2:RSA+AES256-SHA:RSA+AES128-SHA:RSA+RC4-SHA:RSA+RC4-MD5:RSA+DES-CBC3-SHA/ciphers = AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:HIGH:3DES:!PSK:!MD5:!aNULL:!eNULL/' /etc/init.d/xapissl",
+    unless  => "/bin/grep 'AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH:HIGH:3DES:!PSK:!MD5:!aNULL:!eNULL' /etc/init.d/xapissl",
+    require => File['/etc/init.d/xapissl'],
     notify  => Service['xapi'],
   }
 
